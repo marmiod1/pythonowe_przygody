4db38dfee8aae102133c747589e6d4d4d49684e5
marmiod1 <maria.mioduszewska@gazeta.pl>
2018-01-15 14:26:18 +0100

    Update powerOfAnotherInteger.py

--- 08_Co w mojej ocenie jest najtrudniejsze w programowaniu/powerOfAnotherInteger.py
+++ 08_Co w mojej ocenie jest najtrudniejsze w programowaniu/powerOfAnotherInteger.py
@@ -17,4 +17,22 @@ def is_a_power(n, m):
 is_a_power(243, 3)
 
 
+import math
 
+def is_a_power(n, m):
+    while n > m:
+        n = n / m
+        
+    result = n == m # wyrażenie n == m zwraca booleana i to przypisuję do result
+    print(result)
+    return result
+
+is_a_power(1024,2) 
+
+
+def is_a_power(n, m):
+    while n > m:
+        n = n / m
+    return n == m
+
+print(is_a_power(1024,2))
